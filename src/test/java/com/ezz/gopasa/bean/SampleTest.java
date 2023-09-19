0865dffa91d6b888bdb2de8ecf5517e0f7885372
ezzange <ezzang22@naver.com>
2023-09-19 23:18:27 +0900

    SampleTest 작성

---
+++ src/test/java/com/ezz/gopasa/bean/SampleTest.java
@@ -0,0 +1,13 @@
+package com.ezz.gopasa.bean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SampleTest {
+    @Test
+    void test() {
+      //BeaList출력해오기 숙제
+    }
+
+}
